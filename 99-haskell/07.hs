b3fe797bd7865f56ed404471c33831af8697fa7b
Peter Jakubčo <pjakubco@gmail.com>
2016-02-27 12:32:03 +0100

    Add Haskell excercises 07 and 08

---
+++ 99-haskell/07.hs
@@ -0,0 +1,48 @@
+-- Problem 7
+--
+-- (**) Flatten a nested list structure.
+--
+-- Transform a list, possibly holding lists as elements into a `flat' list by
+-- replacing each list with its elements (recursively).
+--
+-- Example:
+--
+-- * (my-flatten '(a (b (c d) e)))
+-- (A B C D E)
+-- Example in Haskell:
+--
+-- We have to define a new data type, because lists in Haskell are homogeneous.
+--
+--  data NestedList a = Elem a | List [NestedList a]
+--  *Main> flatten (Elem 5)
+--  [5]
+--  *Main> flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+--  [1,2,3,4,5]
+--  *Main> flatten (List [])
+--  []
+--
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten (Elem a) = [a]
+flatten (List xs) = concat $ map flatten xs
+
+flatten' (Elem a) = return a
+flatten' (List xs) = xs >>= flatten'
+
+flatten'' (Elem a) = [a]
+flatten'' (List xs) = concatMap flatten'' xs
+
+flatten''' (Elem a) = [a]
+flatten''' (List (x:xs)) = flatten''' x ++ flatten (List xs)
+flatten''' (List []) = []
+
+flatten'''' (Elem a) = return a
+flatten'''' (List xs) = flatten'''' =<< xs
+
+flatten''''' (Elem a) = [a]
+flatten''''' (List xs) = foldr (++) [] $ map flatten''''' xs
+
+
+
+
