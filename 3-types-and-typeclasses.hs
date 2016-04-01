85455c4c918445723fbebd07bb1f5282ddc1e73d
Peter Jakubčo <pjakubco@gmail.com>
2016-04-01 11:44:29 +0200

    Add solutions to Chapter 3

--- 3-types-and-typeclasses.hs
+++ 3-types-and-typeclasses.hs
@@ -19,10 +19,13 @@ data Colour = Red | Orange | Yellow | Green | Blue | Indigo | Violet
  - The Colour typeclass is of type Ord
  - What is the "first" (or least) colour
  -}
-firstColour = undefined
+
+firstColour :: Colour
+firstColour = minimum [minBound..maxBound]  
 
 -- List the colours in reverse order
-reverseColourOrder = undefined
+reverseColourOrder :: [Colour]
+reverseColourOrder = reverse [minBound..maxBound]
 
 {-
  - Mix two colours together, to produce the average value of the two.
@@ -31,4 +34,6 @@ reverseColourOrder = undefined
  - For example: paintMix Green Violet = Indigo
  - Hint: Integer division can be performed with the quot function: quot 7 2 = 3
  -}
-paintMix c1 c2 = undefined
+
+--paintMix :: Colour -> Colour -> Colour
+paintMix c1 c2 = let xs = [(min c1 c2)..(max c1 c2)] in xs !! (length xs `quot` 2)
