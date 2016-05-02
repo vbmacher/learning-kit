42b402e5cde53f447ba60cfe5f96736bb6e9d222
Peter Jakubčo <pjakubco@gmail.com>
2016-05-02 21:11:12 +0200

    Added 6, sumInt

--- learn-you-a-haskell-exercises/6-higher-order-functions.hs
+++ learn-you-a-haskell-exercises/6-higher-order-functions.hs
@@ -2,7 +2,12 @@
 -- Example: sumInts 0 1 = 1
 --          sumInts 1 3 = 6
 sumInts :: Int -> Int -> Int
-sumInts a b = undefined
+sumInts a b = sum [a..b]
+
+sumInts' a b
+  | a == b    = a
+  | otherwise = a + sumInts' (a+1) b
+
 
 -- Define a square function
 sq :: Int -> Int
