e63d63e12e2166fe7a0b3d1d460085a826691c81
Peter Jakubčo <pjakubco@gmail.com>
2016-06-25 09:03:25 +0200

    Add chap.8 for learn-you-a-haskell

--- learn-you-a-haskell-exercises/8-making-our-own-types-and-typeclasses.hs
+++ learn-you-a-haskell-exercises/8-making-our-own-types-and-typeclasses.hs
@@ -12,13 +12,20 @@
  - Uncomment the following declarations to complete the implementation, and provide an implementation for instance Show Card
  -}
 
---data Suit = 
---data Digit = 
---data Card = 
+data Suit = Clubs | Diamonds | Hearts | Spades deriving (Show,Eq)
+data Digit = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten
+             | Jack | Queen | King | Ace deriving (Ord, Eq, Bounded, Enum, Show)
+data Card = Card Digit Suit deriving (Ord,Eq)
+
+instance Ord Suit where
+  compare a b = EQ
+
+instance Show Card where
+  show (Card d s) = "The " ++ show d ++ " of " ++ show s
 
 -- We should be able to provide a function which returns the higher ranked card:
 betterCard :: Card -> Card -> Card
-betterCard x y = undefined
+betterCard x y = if x > y then x else y
 
 -- Here is a new Typeclass, which represents some kind of playing hand in a game.
 -- It returns True for a "winning hand", depending on the rules for the type of class we are playing with
@@ -27,13 +34,13 @@ class Hand a where
 
 -- Implement Hand for Card, where play returns true if the list contains the Ace of Spades
 instance Hand Card where
-    play c = undefined
+    play c = (Card Ace Spades) `elem` c
 
 -- Create a new Coin type
---data Coin = 
+data Coin = Head | Back deriving (Eq)
 
 -- Implement Hand for Coin, where play returns true if there are ten heads in a row in the list
 instance Hand Coin where
-	play c =  undefined
+	play = (==10) . length . takeWhile (==Head) . take 10
 
 -- Have a play with implementing Hand for some other types, for instance Int and Bool
