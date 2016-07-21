4abeea13d63a1588f07ac45cb570762fcfa5cf2b
Peter Jakubčo <pjakubco@gmail.com>
2016-07-21 08:10:40 +0200

    Added more to 17.hs

--- 99-haskell/17.hs
+++ 99-haskell/17.hs
@@ -21,14 +21,35 @@ split' xs 0     = ([], xs)
 split' (x:xs) n = add ([x], []) $ split' xs (n-1)
   where add (as,bs) (cs,ds) = (as ++ cs, bs ++ ds)
 
-
+split'' :: [b] -> Int -> ([b],[b])
 split'' = help ([],[])
   where help (xs,ys) is 0  = (xs, ys ++ is)
         help (xs, ys) [] _ = (xs, ys)
         help (xs, ys) (i:is) n = help (xs ++ [i], ys) is (n-1)
 
 
+split''' xs n = foldl help ([],[]) xs
+  where help (cs,ds) x
+          | length cs == n  = (cs, ds ++ [x])
+          | otherwise       = (cs ++ [x], ds)
 
 
+split'''' xs n = foldr help ([],[]) xs
+  where n' = length xs - n
+        help x (cs,ds)
+          | length ds == n' = (x:cs, ds)
+          | otherwise       = (cs, x:ds)
+
+
+split''''' xs n = foldl help ([],[]) $ zip xs [1..]
+  where help (cs,ds) (x,c)
+          | c > n      = (cs, ds ++ [x])
+          | otherwise  = (cs ++ [x], ds)
+
+
+split'''''' xs n = foldr help ([],[]) $ zip xs [1..]
+  where help (x,c) (cs, ds)
+          | c > n      = (cs, x:ds)
+          | otherwise  = (x:cs, ds)
 
 
