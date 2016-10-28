606be2b83b4171c2f0f238057e9533c6ba30c8c7
Peter Jakubčo <pjakubco@gmail.com>
2016-10-28 13:11:34 +0200

    Fixed in-place quicksort

--- hackerrank/in-place-quicksort/quicksort.rs
+++ hackerrank/in-place-quicksort/quicksort.rs
@@ -1,54 +1,62 @@
 use std::io::{self, BufRead};
 
-fn printvec(v: &Vec<i32>) {
-    for x in v {
-        print!("{} ", x);
-    }
-    println!("");
+fn print(v: &mut Vec<i32>) {
+    println!("{}", v.iter().fold(String::new(), |res, s| {
+        if res.is_empty() {
+            return s.to_string();
+        } else {
+            return res + " " + &s.to_string()
+        }
+    }));
 }
 
-
-fn quicksort(array: &mut Vec<i32>, lo: usize, hi: usize) {
+fn quicksort(v: &mut Vec<i32>, lo:usize, hi:usize) {
     if lo < hi {
-        let p = partition(array, lo, hi);
-        printvec(array);
-        quicksort(array, lo, p - 1);
-        quicksort(array, p + 1, hi);
+        let p = partition(v, lo, hi);
+        print(v);
+        if p > 0 {
+            quicksort(v, lo, p - 1);
+        }
+        quicksort(v, p + 1, hi);
     }
 }
 
-fn partition(array: &mut Vec<i32>, lo: usize, hi: usize) -> usize {
-    let pivot = array[hi];
-    let mut i:usize = lo;
+
+fn partition(v: &mut Vec<i32>, lo: usize, hi: usize) -> usize {
+    let pivot = v[hi];
+    let mut i = lo;
     
     for j in lo..hi {
-        if array[j] <= pivot {
+        if v[j] <= pivot {
             if i != j {
-                array.as_mut_slice().swap(i, j);
+                v.as_mut_slice().swap(i,j);
             }
-            i += 1
+            i += 1;
         }
     }
-    array.as_mut_slice().swap(i, hi);
-    i
+    v.as_mut_slice().swap(i,hi);
+    return i
 }
+
 
 
 fn main() {
     let reader = io::stdin();
-
-    let mut nstr = String::new();
-    reader.read_line(&mut nstr).unwrap();
-    let n = nstr.trim_right().parse::<usize>().unwrap();
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).unwrap();
+    let n:usize = buffer.trim_right().parse().unwrap();
     
-    let mut numbers: Vec<i32> = 
-        reader.lock()
-              .lines().next().unwrap().unwrap()
-              .split(' ').map(|s| s.trim())
-              .filter(|s| !s.is_empty())
-              .map(|s| s.parse().unwrap())
-              .collect();
-  
-    quicksort(&mut numbers, 0, n-1);
+    if n <=0 {
+        return;
+    }
+    
+    let mut numbers: Vec<i32> = reader.lock().lines().next()
+        .unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .collect();
+ 
+    quicksort(&mut numbers, 0, n -1);  
 }
-
