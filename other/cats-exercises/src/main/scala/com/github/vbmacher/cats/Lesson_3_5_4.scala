37d6e4f0ad612c7aec17295bc1694e2f679005f8
Peter Jakubco <pjakubco@gmail.com>
2020-11-30 11:37:48 +0100

    Add more cats lessons exercises

---
+++ other/cats-exercises/src/main/scala/com/github/vbmacher/cats/Lesson_3_5_4.scala
@@ -0,0 +1,27 @@
+package com.github.vbmacher.cats
+
+import cats.Functor
+import cats.syntax.functor._
+
+object Lesson_3_5_4 extends App {
+
+  sealed trait Tree[+A]
+  final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+  final case class Leaf[A](value: A) extends Tree[A]
+
+  object Tree {
+    def branch[A](left: Tree[A], right: Tree[A]): Tree[A] = Branch(left, right)
+    def leaf[A](value: A): Tree[A] = Leaf(value)
+  }
+
+
+  implicit val treeFunctor = new Functor[Tree] {
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match {
+      case Leaf(value) => Leaf(f(value))
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+    }
+  }
+
+  import Tree._
+  println(branch(leaf(10), leaf(20)).map(_ * 2))
+}
